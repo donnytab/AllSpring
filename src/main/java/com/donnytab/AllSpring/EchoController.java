8391ada587194072a549f48ffc6771ed06e67cfb
downson <downson.yuen@gmail.com>
2018-05-27 23:22:49 -0400

    Building a RESTful Web Service

--- src/main/java/com/donnytab/AllSpring/EchoController.java
+++ src/main/java/com/donnytab/AllSpring/EchoController.java
@@ -1,13 +1,24 @@
 package com.donnytab.AllSpring;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class EchoController {
 
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
     @RequestMapping("/")
     public String index() {
         return "Echo Hello!";
+    }
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
