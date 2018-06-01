9fb40fdfca2f297f279f4fad225e74ef979ec89d
downson <downson.yuen@gmail.com>
2018-05-31 23:56:11 -0400

    Consuming RESTful web service

--- src/main/java/com/donnytab/AllSpring/AllSpringApplication.java
+++ src/main/java/com/donnytab/AllSpring/AllSpringApplication.java
@@ -1,14 +1,35 @@
 package com.donnytab.AllSpring;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
 @EnableScheduling
 public class AllSpringApplication {
 
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(AllSpringApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(AllSpringApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+            log.info(quote.toString());
+        };
     }
 }
