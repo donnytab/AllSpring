927c00dac85c383291a97db418e64d3032b62341
downson <downson.yuen@gmail.com>
2018-06-01 23:59:11 -0400

    Access data stored in relational database using Spring JdbcTemplate

--- src/main/java/com/donnytab/AllSpring/AllSpringApplication.java
+++ src/main/java/com/donnytab/AllSpring/AllSpringApplication.java
@@ -1,6 +1,7 @@
 package com.donnytab.AllSpring;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,14 +9,20 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
 
-import java.util.logging.Logger;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @EnableScheduling
 public class AllSpringApplication {
 
     private static final org.slf4j.Logger log = LoggerFactory.getLogger(AllSpringApplication.class);
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(AllSpringApplication.class, args);
@@ -31,5 +38,26 @@ public class AllSpringApplication {
             Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
             log.info(quote.toString());
         };
+    }
+
+    public void run(String... strings) throws Exception {
+        log.info("Creating tables");
+
+        jdbcTemplate.execute("DROP TABLE customers IF EXISTS");
+        jdbcTemplate.execute("CREATE TABLE customers(id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+
+        List<Object[]> splitUpNames = Arrays.asList("John Wood", "Nelson Craw").stream()
+                .map(name->name.split(" "))
+                .collect(Collectors.toList());
+
+        splitUpNames.forEach(name->log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
+
+        jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
+
+        log.info("Querying for customer records where first_name = 'John':");
+
+        jdbcTemplate.query("SELECT id, first_name, last_name FROM customers WHERE first_name=?", new Object[]{"John"},
+                (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
+        ).forEach(customer -> log.info(customer.toString()));
     }
 }
