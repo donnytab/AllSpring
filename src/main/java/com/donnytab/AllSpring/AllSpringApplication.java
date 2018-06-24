ac0f4b29f6dea855e58f01bb3b5a3a1a234df12b
downson <downson.yuen@gmail.com>
2018-06-23 23:37:52 -0400

    Access data in Pivotal GemFire

--- src/main/java/com/donnytab/AllSpring/AllSpringApplication.java
+++ src/main/java/com/donnytab/AllSpring/AllSpringApplication.java
@@ -1,5 +1,6 @@
 package com.donnytab.AllSpring;
 
+import org.apache.geode.cache.client.ClientRegionShortcut;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -7,6 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.gemfire.config.annotation.ClientCacheApplication;
+import org.springframework.data.gemfire.config.annotation.EnableEntityDefinedRegions;
+import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
@@ -18,6 +22,12 @@ import java.util.stream.Collectors;
 
 @SpringBootApplication
 @EnableScheduling
+
+//@ClientCacheApplication(name = "AccessingDataGemFireApplication", logLevel = "error")
+//@EnableEntityDefinedRegions(basePackageClasses = Person.class,
+//        clientRegionShortcut = ClientRegionShortcut.LOCAL)
+//@EnableGemfireRepositories
+
 //public class AllSpringApplication {
 public class AllSpringApplication implements CommandLineRunner {
 
@@ -62,4 +72,48 @@ public class AllSpringApplication implements CommandLineRunner {
                 (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
         ).forEach(customer -> log.info(customer.toString()));
     }
+
+
+    /* Accessing Data in Pivotal GemFire
+
+    @Bean
+    ApplicationRunner run(PersonRepository personRepository) {
+
+        return args -> {
+
+            Person alice = new Person("Alice", 40);
+            Person bob = new Person("Baby Bob", 1);
+            Person carol = new Person("Teen Carol", 13);
+
+            System.out.println("Before accessing data in Pivotal GemFire...");
+
+            asList(alice, bob, carol).forEach(person -> System.out.println("\t" + person));
+
+            System.out.println("Saving Alice, Bob and Carol to Pivotal GemFire...");
+
+            personRepository.save(alice);
+            personRepository.save(bob);
+            personRepository.save(carol);
+
+            System.out.println("Lookup each person by name...");
+
+            asList(alice.getName(), bob.getName(), carol.getName())
+              .forEach(name -> System.out.println("\t" + personRepository.findByName(name)));
+
+            System.out.println("Query adults (over 18):");
+
+            stream(personRepository.findByAgeGreaterThan(18).spliterator(), false)
+              .forEach(person -> System.out.println("\t" + person));
+
+            System.out.println("Query babies (less than 5):");
+
+            stream(personRepository.findByAgeLessThan(5).spliterator(), false)
+              .forEach(person -> System.out.println("\t" + person));
+
+            System.out.println("Query teens (between 12 and 20):");
+
+            stream(personRepository.findByAgeGreaterThanAndAgeLessThan(12, 20).spliterator(), false)
+              .forEach(person -> System.out.println("\t" + person));
+        };
+    }*/
 }
